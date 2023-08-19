f0b4a6fbf82c21505a8f6bfc220d98deae950ad3
arsenkhoIod <arsenykholod@yandex.ru>
2023-08-19 20:03:23 +0300

    1 step

--- colorized_app_2/ViewController.swift
+++ colorized_app_2/ViewController.swift
@@ -9,11 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 15
+        
+        setColor()
+        
+        setValue(for: redLabel, greenLabel, blueLabel)
     }
+    
+    
+    @IBAction func sliderColorView(_ sender: UISlider) {
+        
+        setColor()
+        
+        setValue(for: redLabel, greenLabel, blueLabel)
+        
+    }
+        
+    private func string(from slider: UISlider) -> String{
+        String(format: "%.2f", slider.value)
+    }
+            
 
-
+    
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        for label in labels {
+            switch label{
+            case redLabel:
+                redLabel.text = string(from: redSlider)
+            case greenLabel:
+                greenLabel.text = string(from: greenSlider)
+            default:
+                blueLabel.text = string(from: blueSlider)
+            }
+        }
+    }
+    
 }
 
